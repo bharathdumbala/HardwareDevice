6770ebf11092640a1e3c5b6067908b665bd69b74
bharathdumbala <bharathdumbala@outlook.com>
2023-06-30 20:00:53 +0530

    commit 30-06-2023

---
+++ LeadingResponse/LeadingResponse.Android/MainActivity.cs
@@ -0,0 +1,77 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Prism;
+using Prism.Ioc;
+using Android.Support.V4.App;
+using Android.Support.V4.Content;
+using Android;
+using System;
+using Acr.UserDialogs;
+
+namespace LeadingResponse.Droid
+{
+    [Activity(Theme = "@style/MainTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait,
+              ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            ZXing.Net.Mobile.Forms.Android.Platform.Init();
+            LoadApplication(new App(new AndroidInitializer()));
+            UserDialogs.Init(this);
+
+            if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.Camera) != Permission.Granted)
+            {
+                // Request the camera permission
+                ActivityCompat.RequestPermissions(this, new[] { Manifest.Permission.Camera }, 1);
+            }
+            else
+            {
+                // Permission already granted, proceed with app initialization
+                InitializeApp();
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            if (requestCode == 1)
+            {
+                if (grantResults.Length > 0 && grantResults[0] == Permission.Granted)
+                {
+                    // Permission granted, proceed with app initialization
+                    InitializeApp();
+                }
+                else
+                {
+                    // Permission denied, handle accordingly (e.g., show a message, disable related functionality)
+                    ShowPermissionDeniedMessage();
+                }
+            }
+        }
+
+        private void ShowPermissionDeniedMessage()
+        {
+        }
+
+        private void InitializeApp()
+        {
+        }
+
+        public class AndroidInitializer : IPlatformInitializer
+        {
+            public void RegisterTypes(IContainerRegistry containerRegistry)
+            {
+                // Register any platform specific implementations
+            }
+        }
+
+
+    }
+}
---
+++ LeadingResponse/LeadingResponse/ViewModels/ViewModelBase.cs
@@ -0,0 +1,93 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeadingResponse.ViewModels
+{
+    public class ViewModelBase : BindableBase, IInitialize, INavigationAware, IDestructible
+    {
+        protected INavigationService NavigationService { get; private set; }
+
+        private string _image;
+        public string Image
+        {
+            get { return _image; }
+            set { SetProperty(ref _image, value); }
+        }
+        private string _scannedData;
+        public string ScannedData
+        {
+            get { return _scannedData; }
+            set { SetProperty(ref _scannedData, value); }
+        }
+
+        private string _badgeid = "Unassigned";
+        public string BadgeId
+        {
+            get { return _badgeid; }
+            set { SetProperty(ref _badgeid, value); }
+        }
+
+        private string _equipmentid = "Unassigned";
+        public string EquipmentId
+        {
+            get { return _equipmentid; }
+            set { SetProperty(ref _equipmentid, value); }
+        }
+
+        private string _workorderid = "Unassigned";
+        public string WorkOrderId
+        {
+            get { return _workorderid; }
+            set { SetProperty(ref _workorderid, value); }
+        }
+
+        private string _sample;
+        public string Sample
+        {
+            get { return _sample; }
+            set { SetProperty(ref _sample, value); }
+        }
+
+        private string _responsedata;
+        public string ResponseData
+        {
+            get { return _responsedata; }
+            set { SetProperty(ref _responsedata, value);}
+        }
+
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set { SetProperty(ref _message, value); }
+        }
+        public ViewModelBase(INavigationService navigationService)
+        {
+            NavigationService = navigationService;
+        }
+
+        public virtual void Initialize(INavigationParameters parameters)
+        {
+
+        }
+
+        public virtual void OnNavigatedFrom(INavigationParameters parameters)
+        {
+
+        }
+
+        public virtual void OnNavigatedTo(INavigationParameters parameters)
+        {
+
+        }
+
+        public virtual void Destroy()
+        {
+
+        }
+    }
+}
